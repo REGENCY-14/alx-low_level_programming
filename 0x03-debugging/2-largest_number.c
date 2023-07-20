9729554746a0d519e20ca475f91912446624ed4e
REGENCY-14 <osmantimtonizakaria14@gmail.com>
2023-07-20 23:22:11 +0000

    hey

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,15 +14,16 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && b > c)
 	{
-	largest = a;
+		largest = a;
 	}
-	else if (b > a && a > c)	
+	else if (b > a && a > c)
 	{
-	largest = b;
+		largest = b;
 	}
-	else if (c > a && a > b)
+
+	else if (c >= a && a >= b)
 	{
-	largest = c;
+		largest = c;
 	}
 
 	return (largest);
